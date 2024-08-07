91d2a24c63eff493861e683fea5a3663dbb296b7
Sam Shulman <sshulma5@mail.yu.edu>
2024-08-07 01:12:49 -0400

    socials

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -29,7 +29,7 @@ const navigation = {
   social: [
     {
       name: "Facebook",
-      href: "#",
+      href: "https://www.facebook.com/share/3LoEDricwdQNYGTj/?mibextid=LQQJ4d",
       icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -42,7 +42,7 @@ const navigation = {
     },
     {
       name: "Instagram",
-      href: "#",
+      href: "https://www.instagram.com/kitahub.io/",
       icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -55,7 +55,7 @@ const navigation = {
     },
     {
       name: "X",
-      href: "#",
+      href: "https://x.com/kita_hubIO",
       icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
@@ -89,7 +89,7 @@ export default function Footer() {
               {navigation.social.map((item) => (
                 <a
                   key={item.name}
-                  href="about-us"
+                  href={item.href}
                   className="text-gray-500 hover:text-gray-400"
                 >
                   <span className="sr-only">{item.name}</span>
