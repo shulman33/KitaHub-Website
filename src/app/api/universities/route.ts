0b0763d6014abe67420d0786b59b5da229ea9d65
Sam Shulman <sshulma5@mail.yu.edu>
2024-10-31 13:17:00 -0400

    docs

---
+++ src/app/api/universities/route.ts
@@ -0,0 +1,11 @@
+export async function GET(_request: Request) {
+  return new Response("Not Implemented", {
+    status: 501,
+  });
+}
+
+export async function POST(_request: Request) {
+  return new Response("Not Implemented", {
+    status: 501,
+  });
+}
