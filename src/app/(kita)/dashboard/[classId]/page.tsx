53cbc18b0a1f3f3e61f7a84f565ebad65fab7005
Sam Shulman <sshulma5@mail.yu.edu>
2024-12-29 01:49:18 -0500

    Update page.tsx

--- src/app/(kita)/dashboard/[classId]/page.tsx
+++ src/app/(kita)/dashboard/[classId]/page.tsx
@@ -1,4 +1,4 @@
-import { getSession } from "@auth0/nextjs-auth0/edge";
+import { getSession } from "@auth0/nextjs-auth0";
 import StudentClassDashboard from "../../components/StudentComponents/StudentClassDashboard";
 import ProfessorClassDashboard from "../../components/ProfessorComponents/ProfessorClassDashboard";
 import { getUserRole } from "../../lib/auth";
