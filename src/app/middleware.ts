66fe862654c5bb64c081d549acf6a384c4e12af6
Sam Shulman <sshulma5@mail.yu.edu>
2024-12-30 12:30:25 -0500

    try again

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -3,5 +3,10 @@ import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: "/dashboard/:path*",
+  matcher: [
+    "/dashboard/:path*",
+    "/dashboard/[classId]/:path*",
+    "/dashboard/[classId]/discussions/:path*",
+    "/dashboard/[classId]/discussions/[messageId]/:path*",
+  ],
 };
