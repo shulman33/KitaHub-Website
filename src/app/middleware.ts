6be1c50efc60fe64ec67a14a3567c434a2e0a053
Sam Shulman <sshulma5@mail.yu.edu>
2024-12-30 12:44:39 -0500

    last chance

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -3,10 +3,5 @@ import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/dashboard/[classId]/:path*",
-    "/dashboard/[classId]/discussions/:path*",
-    "/dashboard/[classId]/discussions/[messageId]/:path*",
-  ],
+  matcher: ["/(kita)/dashboard/:path*"],
 };
