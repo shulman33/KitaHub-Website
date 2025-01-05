28a63e9fa6e937b211d520cc71403e5c08453cec
Sam Shulman <sshulma5@mail.yu.edu>
2025-01-05 00:03:13 -0500

    created user api

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,5 +1,70 @@
-import { NextResponse, type NextRequest } from 'next/server';
+import { db } from "@/app/db/drizzle";
+import { user } from "@/app/db/schema";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-    return NextResponse.json({ message: 'Hello, world!' });
+  try {
+    console.log("Received request to create user record.");
+
+    const data = await req.json();
+    console.log("Request body: ", data);
+
+    const {
+      auth0UserId,
+      firstName,
+      lastName,
+      role,
+      schoolEmail,
+      universityId,
+      profilePicture,
+      primaryEmail,
+    } = data;
+
+    if (
+      !auth0UserId ||
+      !firstName ||
+      !lastName ||
+      !role ||
+      !schoolEmail ||
+      !universityId ||
+      !primaryEmail
+    ) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+
+    const userRole = role === "Student" ? "STUDENT" : "PROFESSOR";
+
+    try {
+      await db.insert(user).values({
+        auth0UserId: auth0UserId,
+        firstName: firstName,
+        lastName: lastName,
+        role: userRole,
+        schoolEmail: schoolEmail,
+        email: primaryEmail,
+        universityId: universityId,
+        profilePicture: profilePicture,
+      });
+    } catch (error) {
+      console.error("Error creating user record:", error);
+      return NextResponse.json(
+        { error: "Failed to create user record" },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "User record created" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error creating user record:", error);
+    return NextResponse.json(
+      { error: "Failed to create user record" },
+      { status: 500 }
+    );
+  }
 }
