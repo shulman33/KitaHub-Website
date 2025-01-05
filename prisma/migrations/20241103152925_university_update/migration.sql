c8f61ab4f44313580a914c108500fa4ce9a5a4b1
Sam Shulman <sshulma5@mail.yu.edu>
2025-01-04 20:39:47 -0500

    removed prisma references

--- prisma/migrations/20241103152925_university_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name]` on the table `University` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "University_name_key" ON "University"("name");
